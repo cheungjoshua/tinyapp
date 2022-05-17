bcf8a563100e4f9d62563539d722edb022a3cdbc
Joshua Cheung <contact.cjoshua@gmail.com>
2022-05-16 18:58:56 -0700

    finish the basic interface display

--- express_server.js
+++ express_server.js
@@ -13,11 +13,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// app.get("/hello", (req, res) => {
-//   const templateVars = { greeting: "Hello World!" };
-//   res.render("hello_world", templateVars);
-// });
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
